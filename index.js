36c81f5f9212d6367e9525a0c864da1c1b94f031
saguiguilid <tbki.jea@gmail.com>
2019-09-24 14:01:35 +0800

    2.4: Constructor

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 function createCircle(radius) {
   return {
     radius,
-    draw: function()
-    {
-      console.log("draw")
+    draw: function() {
+      console.log("draw from factory function");
     }
-  }
+  };
 }
 
 const circle = createCircle(1);
 circle.draw();
+
+// Constructor Function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw from constructor function");
+  };
+}
+
+const another = new Circle(1);
+another.draw();
