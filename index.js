168191bd9ba0e87a326b68c15476240e9355656b
saguiguilid <tbki.jea@gmail.com>
2019-09-24 14:30:55 +0800

    2.9: Enumerating Properties

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// Factory Function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function() {
-      console.log("draw from factory function");
-    }
-  };
-}
-
-const circle = createCircle(1);
-circle.draw();
-
-// Constructor Function
 function Circle(radius) {
   this.radius = radius;
   this.draw = function() {
-    console.log("draw from constructor function");
+    console.log("draw");
   };
 }
 
-const another = new Circle(1);
-another.draw();
+const circle = new Circle(10);
+
+// get all properties
+for (let key in circle) {
+  if (typeof circle[key] !== "function") {
+    console.log(key, circle[key]);
+  }
+}
+
+// another way of getting properties
+const keys = Object.keys(circle);
+console.log(keys);
+
+// has given property?
+if ("radius" in circle) {
+  console.log("Circle has a radius");
+}
