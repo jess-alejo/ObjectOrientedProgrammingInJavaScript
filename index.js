c3eafa3807cf57b558bfda16dd8d037b0e4dc2e0
saguiguilid <tbki.jea@gmail.com>
2019-09-24 13:53:18 +0800

    2.3: Factories

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function()
-  {
-    console.log("draw")
+// Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function()
+    {
+      console.log("draw")
+    }
   }
 }
 
+const circle = createCircle(1);
 circle.draw();
